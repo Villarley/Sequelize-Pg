f8a77da1eed85ae9c0d284dfb26598ff951c38e4
Santiago Villarreal <santivillarley1010@gmail.com>
2023-07-06 11:29:24 -0600

    Connected to PostgreSQL

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import app from './app.js';
-
+import { sequelize } from './database/database.js';
 let port = 3060
 
-app.listen(port)
-
-console.log(`Server running on ${port} port`); 
+async function main(){
+    try{
+        await sequelize.authenticate();
+        console.log('Connection has been established succesfully');
+        app.listen(3060);
+        console.log('Server is running on '+ 3060);
+    } catch(err){
+        console.log('Unable to connect', err);
+    }
+}
+main();
